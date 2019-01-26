36bc5d591a85a96350d733d9187415f7c6946f75
Emanuele Garolla <e.garolla@reply.it>
2019-01-26 21:06:11 +0100

    Lez67 - Strating transaction

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 #Initializing our blockchain
 blockchain = []
+open_transactions = []  
+owner = "Ema"
 
 def get_last_blockchain_value():
     if len(blockchain) < 1:
@@ -7,16 +9,20 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(recipient, sender = owner, amount=1.0):
     """ WOWOWOWOWOWOWO """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {"sender" : sender, "recipient" : recipient, "amount" : amount }
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    pass
 
 
 def get_transaction_value():
-    user_input = float(input("Your transaction amount please: "))
-    return user_input
+    tx_recipient = input("Enter the recipient of the transaction: ")
+    tx_amount = float(input("Your transaction amount please: "))
+    return (tx_recipient, tx_amount)
     
 
 def get_user_choice():
@@ -56,8 +62,9 @@ while waiting_for_input:
 
     user_choice = get_user_choice()
     if user_choice == "1":
-        tx_amount = get_transaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount) 
     elif user_choice == "2": 
         print_blockchain_elements()
     elif user_choice == "h":
