be09d227e166d66ddaca18eb44e184bf5c0f2940
Emanuele Garolla <e.garolla@reply.it>
2019-02-28 19:21:00 +0100

    Parse tags

--- HashCode.py
+++ HashCode.py
@@ -9,8 +9,12 @@ with open('./a_example.txt', 'r') as f:
     for line in f:
         temp = line.strip('\n')
         value = temp.split(' ')
-        if len(value) >= 4:
-            print(value[0] + " " + value[1] + " " + value[2] + " " + value[3])
+        if len(value) >= 2:
+            position = value[0]
+            num_of_tags = value[1]
+            tags = value[2:len(value)]
+            print(position + " " + num_of_tags)
+            print(tags)
             # outfile.write(value[1])
 
     outfile.write("""3\n0\n3\n1 2""")
