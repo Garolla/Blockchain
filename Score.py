2bd895230d4e8acbf996318ca98ec8310c2ffc0f
Davide Destefanis <d.destefanis@reply.it>
2019-02-28 21:05:04 +0100

    add totalScore function

--- Score.py
+++ Score.py
@@ -16,3 +16,12 @@ def score(sl1=Slide(),sl2=Slide()):
     diffA = sl1.tags.difference(sl2.tags)
     diffB = sl2.tags.difference(sl1.tags)
     return min(intersCount,len(diffA),len(diffB))
+
+
+def totalScore(slides):
+    total = 0
+    i = 0
+    while i < len(slides) - 1:
+        total += score(slides[i], slides[i+1])
+        i += 1
+    return total
